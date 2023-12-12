8b65234730e02344d13f9eb1ff8cb2a7538e1ae0
Bagustyo <bagustyo@gmail.com>
2023-12-12 16:52:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,21 @@
 
 import streamlit as st
 
-title = st.text_input('Movie title', 'Life of Brian')
-st.write('The current movie title is', title)
+def main():
+    # Set the title text
+    st.title("Streamlit App")
+
+    # Input text
+    input_text = st.text_input("Enter Text:")
+
+    # Dropdown with options 'submitted' and 'deleted'
+    status_options = ['submitted', 'deleted']
+    selected_status = st.selectbox("Select Status:", status_options)
+
+    # Button to trigger an action
+    if st.button("Submit"):
+        # Action to perform when the button is clicked
+        st.success(f"Text: {input_text}, Status: {selected_status} Submitted!")
+
+if __name__ == "__main__":
+    main()
